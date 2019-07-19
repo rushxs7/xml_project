8cbc69a55a0dd51ce8819ba9386da537e5571e8d
Rushil Ramautar <rushil.ramautar@gmail.com>
2019-07-19 12:50:24 -0300

    added fontawesome

---
+++ resources/modules/importbar.php
@@ -0,0 +1,18 @@
+<div class="row my-2">
+    <div class="col">
+        <div class="p-3 bg-light rounded">
+            
+            <h5>Import RSS/XML feed</h5>
+
+            <form action="app/import_rss.php">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="import rss/xml url here...">
+                    <div class="input-group-append">
+                        <button class="btn btn-success" type="submit">Import</button>
+                    </div>
+                </div>
+            </form> 
+
+        </div>
+    </div>
+</div>
---
+++ resources/modules/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">RSS Project</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor03">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Saved</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tutorial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">What is RSS?</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
