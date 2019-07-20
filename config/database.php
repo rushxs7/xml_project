39f2af1920a9d4b181ea248f4a79fc7735f5c3de
Rushil Ramautar <rushil.ramautar@gmail.com>
2019-07-20 16:10:27 -0300

    reverted to mysqli

--- config/database.php
+++ config/database.php
@@ -8,4 +8,8 @@ $database = 'xml_project';
 
 $connection = mysqli_connect($host, $username, $password, $database);
 
+if(!$connection){
+    die("Connection failed: " . mysqli_connect_error($connection));
+}
+
 ?>
