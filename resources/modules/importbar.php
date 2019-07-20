170b5717e27c0a66877788bbf1a1754a302eb7e0
Rushil Ramautar <rushil.ramautar@gmail.com>
2019-07-20 09:39:03 -0300

    added error and success code

--- resources/modules/importbar.php
+++ resources/modules/importbar.php
@@ -4,15 +4,26 @@
             
             <h5>Import RSS/XML feed</h5>
 
-            <form action="app/import_rss.php">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="import rss/xml url here...">
+            <form action="app/import_rss.php" method="POST">
+                <div class="input-group <?php if(isset($_SESSION['rss_failed'])){echo('has-danger');} if(isset($_SESSION['rss_success'])){echo('has-success');} ?>">
+                    <input type="text" class="form-control <?php if(isset($_SESSION['rss_failed'])){echo('is-invalid');}if(isset($_SESSION['rss_success'])){echo('is-valid');} ?>" placeholder="import rss/xml url here...">
                     <div class="input-group-append">
-                        <button class="btn btn-success" type="submit"><i class="fas fa-file-import"></i>&nbsp;&nbsp;Import</button>
+                        <button class="btn btn-primary" type="submit"><i class="fas fa-file-import"></i>&nbsp;&nbsp;Import</button>
                     </div>
+                    <?php if(isset($_SESSION['rss_failed'])){echo('<div class="invalid-feedback">The validation has failed.</div>');} ?>
+                    <?php if(isset($_SESSION['rss_success'])){echo('<div class="valid-feedback">The validation has passed.</div>');} ?>
                 </div>
-            </form> 
+            </form>
 
         </div>
     </div>
 </div>
+
+<?php
+    if(isset($_SESSION['rss_failed'])){
+        unset($_SESSION['rss_failed']);
+    }
+    if(isset($_SESSION['rss_success'])){
+        unset($_SESSION['rss_success']);
+    }
+?>
