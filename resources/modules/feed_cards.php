95b325fee5bed90d30912ed3f2858597df2770bb
Rushil Ramautar <rushil.ramautar@gmail.com>
2019-07-20 12:38:58 -0300

    modularized feed cards

---
+++ resources/modules/feed_cards.php
@@ -0,0 +1,19 @@
+<?php
+
+    require 'config/database.php';
+
+?>
+<div class="col-lg-6 col-md-12">
+    <div class="card border-primary mb-4 shadow">
+        <div class="card-header">Starnieuws</div>
+        <div class="card-body">
+            <p class="card-text">
+                <ul>
+                    <li>Headline 1</li>
+                    <li>Headline 2</li>
+                    <li>Headline 3</li>
+                </ul>
+            </p>
+        </div>
+    </div>
+</div>
