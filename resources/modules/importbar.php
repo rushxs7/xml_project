d83dc871c54befee6d6ca24b7e8e90af6799a921
Rushil Ramautar <rushil.ramautar@gmail.com>
2019-07-20 23:39:10 -0300

    changed button color scheme

--- resources/modules/importbar.php
+++ resources/modules/importbar.php
@@ -8,7 +8,7 @@
                 <div class="input-group">
                     <input id="importUrl" type="text" class="form-control" placeholder="import rss/xml url here...">
                     <div class="input-group-append">
-                        <button id="importButton" class="btn btn-primary" onclick="event.preventDefault();"><i id="importIcon" class="fas fa-file-import"></i><i id="loadingIcon" class="fas fa-circle-notch fa-spin" style="display: none;"></i>&nbsp;&nbsp;Import</button>
+                        <button id="importButton" class="btn btn-primary" onclick="event.preventDefault();"><i id="importIcon" class="fas fa-file-import"></i><i id="loadingIcon" class="fas fa-sync fa-spin" style="display: none;"></i>&nbsp;&nbsp;Import</button>
                     </div>
                 </div>
             </form>
