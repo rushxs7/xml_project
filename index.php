8a0269b20682dc0a3547a04b7329c77eae19ccc4
Rushil Ramautar <rushil.ramautar@gmail.com>
2019-07-21 11:47:52 -0300

    added favicon

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/all.css"><!-- FontAwesome 5.9.0 -->
     <link rel="stylesheet" href="assets/css/loader.css"><!-- Loader Animation CSS -->
