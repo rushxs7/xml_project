58e6ecfea3bcf4a079eca73f28cf87458fd3e3e5
Rushil Ramautar <rushil.ramautar@gmail.com>
2019-07-21 18:08:38 -0300

    changed page titles

--- detail.php
+++ detail.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
+    <title>RSS Project - Details</title>
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/all.css"><!-- FontAwesome 5.9.0 -->
